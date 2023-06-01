18e881ce62d3093296592aacc05d8b2e0a573c0d
VINICIUS OLIVEIRA COSTA <vinicius.ocosta3@senacsp.edu.br>
2023-06-01 17:13:50 -0300

    Segundo commit

---
+++ MaquinaDeDoces/ControlPagamento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaquinaDeDoces
+{
+    class ControlPagamento
+    {
+
+        Pagamento pgto;
+        public ControlPagamento() 
+        {
+            pgto= new Pagamento();  
+        }//fim do construtor
+    }//fim de classe
+}//fim de metodo 
